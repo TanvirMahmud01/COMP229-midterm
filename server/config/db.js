90371b33507e34accd47cdfe861a0210905ac93d
Tanvir Mahmud <tonmoy_0101@gmail.com>
2022-10-27 18:44:18 -0400

    Update DB for MongoDB Atlas integration

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://127.0.0.1/books229"
+  //"URI": "mongodb://127.0.0.1/books229"
+  "URI": "mongodb+srv://Tanvir:123@mongodb-comp229-midterm.ryjzrpg.mongodb.net/?retryWrites=true&w=majority"  
 };
